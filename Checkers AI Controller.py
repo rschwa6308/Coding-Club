e9338c75515565b1dc7557cb935eb648711e38dc
Russell Schwartz <rschwa6308@inst.hcpss.org>
2017-05-02 14:23:36 -0400

    Reorganization

--- Checkers AI Controller.py
+++ Checkers AI Controller.py
@@ -1,61 +1,7 @@
 from copy import deepcopy
-
 from itertools import combinations
 
-
-
-
-
-
-def is_legal_advance(board, (start, end)):
-    if board[start[1]][start[0]] == 1:
-        return abs(end[0] - start[0]) == 1 and (end[1] - start[1] == 1)
-    else:
-        return abs(end[0] - start[0]) == 1 and abs(end[1] - start[1]) == 1
-
-
-def is_legal_jump(board, (start, end)):
-    print start, end
-    if board[(start[1] + end[1])/2][(start[0] + end[0])/2] in (2, 20):
-        if board[start[1]][start[0]] == 1:
-            if abs(end[0] - start[0]) == 2 and (end[1] - start[1] == 2):
-                print "True!", start, end
-                return True
-            else:
-                print "False!", start, end
-                return False
-        else:
-            return abs(end[0] - start[0]) == 2 and abs(end[1] - start[1]) == 2
-    else:
-        return False
-
-
-
-def is_legal_move(board, move):
-    # If any square is off the board, return false
-    if any(min(x, y) < 0 or max(x, y) > 7 for (x, y) in move):
-        return False
-    
-    # If starting square is not an owned piece, return false
-    if board[move[0][1]][move[0][0]] not in (1, 10):
-        return False
-    
-    # If any intermediate square is occupied, return false
-    if any(board[y][x] != 0 for (x, y) in move[1:]):
-        return False
-
-    # If move is a basic advance, check it
-    if len(move) == 2 and abs(move[0][0] - move[1][0]) == 1:
-        return is_legal_advance(board, move)
-
-    # Check jump legality
-    board_copy = deepcopy(board)
-    for start, end in [(move[i], move[i+1]) for i in range(len(move) - 1)]:
-        board_copy = apply_move(board_copy, [start, end])
-        if not is_legal_jump(board_copy, [start, end]):
-            return False
-
-    return True
+from CheckersFunctions import *
 
 
 
@@ -70,21 +16,6 @@ class Ai:
         self.func = func
     def get_move(self, board):
         return self.func(board)
-
-
-
-
-def apply_move(board, move):
-    start_value = board[move[0][1]][move[0][0]]
-    board[move[0][1]][move[0][0]] = 0
-    if len(move) == 2 and abs(move[0][0] - move[1][0]) == 1:
-        board[move[1][1]][move[1][0]] = start_value
-    else:
-        for i in range(len(move) - 1):
-            start = move[i]
-            end = move[i + 1]
-            board[(start[1] + end[1]) / 2][(start[0] + end[0]) / 2] = 0
-        board[move[-1][1]][move[-1][0]] = start_value
 
 
 
@@ -106,7 +37,8 @@ def flipped_board(board):
 
 
 def display_board(board):
-    print "\n".join([" ".join([(str(cell), ".")[cell == 0] for cell in row]) for row in board])
+    legend = {0: ".", 1: "x", 10: "X", 2: "o", 20: "O"}
+    print "\n".join([" ".join([legend[cell] for cell in row]) for row in board])
 
 
 
@@ -115,27 +47,37 @@ def run_game(ai_a, ai_b):
         [0, 1, 0, 1, 0, 1, 0, 1],
         [1, 0, 1, 0, 1, 0, 1, 0],
         [0, 1, 0, 1, 0, 1, 0, 1],
-        [0, 0, 2, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
         [2, 0, 2, 0, 2, 0, 2, 0],
-        [0, 2, 0, 2, 0, 0, 0, 2],
+        [0, 2, 0, 2, 0, 2, 0, 2],
         [2, 0, 2, 0, 2, 0, 2, 0]
     ]
 
     display_board(board)
     for i in range(100):
-        for ai in (ai_a, ai_b):        
+        for ai in (ai_a, ai_b):
+            print "\n\n"
+
+            if not has_moves(board):
+                print "Stalemate!"
+                return None
+            
             move = ai.get_move(board)
-            print "\n\nmove: " + str(move)
+            # print "\n\nmove: " + str(move)
             if is_legal_move(board, move):
                 apply_move(board, move)
             else:
                 print "Illegal move made by " + ai.name
-            display_board(board)
+
+            if is_game_won(board):
+                return ai
+                
+            if ai is ai_a:
+                display_board(board)
+            else:
+                display_board(flipped_board(board))
             board = flipped_board(board)
-
-
-
 
 
 
@@ -147,42 +89,21 @@ def run_game(ai_a, ai_b):
 
 if __name__ == "__main__":
 
-    board = [
-        [0, 1, 0, 1, 0, 1, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0],
-        [0, 1, 0, 0, 0, 1, 0, 1],
-        [0, 0, 2, 0, 2, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [2, 0, 2, 0, 2, 0, 2, 0],
-        [0, 2, 0, 2, 0, 0, 0, 2],
-        [2, 0, 2, 0, 2, 0, 2, 0]
-    ]
-
-    unit_tests = [
-        ([(1, 2), (3, 4), (5, 6)], True),
-        ([(1, 12), (2, 3)], False),
-        ([(1, 2), (2, -3)], False),
-        ([(1, 2), (2, 3), (3, 4)], False),
-        ([(1, 4), (3, 2)], False),
-        ([(1, 4), (3, 2), (5, 4)], False),
-        ([(1, 2), (3, 4), (5, 6)], True),
-        ([(1, 2), (3, 4), (5, 6), (3, 4)], False)
-    ]
-
-    for test in unit_tests:
-        result = is_legal_move(board, test[0])
-        print str(test[0]) + " --> " + str(result) + "\t" + ("PASS" if result == test[1] else "FAIL")
 
     from LiamCheckers import makeMove
     from TestAIs import *
     
-##    ais = [
-##        Ai("Liam 1", makeMove),
-##        Ai("Liam 2", makeMove)
-##    ]
-##
-##    combs = combinations(ais, 2)
-##
-##    for pair in combs:
-##        victor = run_game(pair[0], pair[1])
-##        print victor.name + " wins"
+    ais = [
+        Ai("Russell 1", random_ai),
+        Ai("Russell 2", random_ai)
+    ]
+
+    combs = combinations(ais, 2)
+
+    for pair in combs:
+        victor = run_game(pair[0], pair[1])
+        if victor is not None:
+            print victor.name + " wins!"
+        else:
+            print "Tie!"
+
--- TestAIs.py
+++ TestAIs.py
@@ -1,92 +1,12 @@
 from random import choice
 from copy import deepcopy
 
+from CheckersFunctions import *
+
 
 def random_ai(board):
     return choice(get_moves(board))
 
-
-def get_jumps(board, open_, closed):
-    print "open_: ", open_
-    new_open_ = deepcopy(open_)
-    for move in open_:
-        print move
-        current_move_changed = False
-        last = move[-1]
-        for shift_y in (-2, 2):
-            for shift_x in (-2, 2):
-                new = deepcopy(move)
-                new.append((last[0] + shift_x, last[1] + shift_y))
-                print "checking new: " + str(new) 
-                if is_legal_move(board, new):
-                    current_move_changed = True
-                    new_open_.append(new)    
-        new_open_.remove(move)
-        closed.append(move)
-
-    if len(new_open_) == 0:
-        return closed
-    else:
-        return get_jumps(board, new_open_, closed)
-
-
-
-def get_moves(board):
-    moves = []
-    for y in range(len(board)):
-        for x in range(len(board[y])):
-            if board[y][x] in (1, 10):
-                # print x, y
-                if is_legal_move(board, [(x, y), (x - 1, y + 1)]):
-                    moves.append([(x, y), (x - 1, y + 1)])
-                if is_legal_move(board, [(x, y), (x + 1, y + 1)]):
-                    moves.append([(x, y), (x + 1, y + 1)])
-    return moves
-
-
-def is_legal_advance(board, (start, end)):
-    if board[start[1]][start[0]] == 1:
-        return abs(end[0] - start[0]) == 1 and (end[1] - start[1] == 1)
-    else:
-        return abs(end[0] - start[0]) == 1 and abs(end[1] - start[1]) == 1
-
-
-def is_legal_jump(board, (start, end)):
-    if board[(start[1] + end[1])/2][(start[0] + end[0])/2] in (2, 20):
-        if board[start[1]][start[0]] == 1:
-            print "is legal jump: ", start, end
-            return abs(end[0] - start[0]) == 2 and (end[1] - start[1] == 2)
-        else:
-            return abs(end[0] - start[0]) == 2 and abs(end[1] - start[1]) == 2
-    else:
-        return False
-
-
-
-def is_legal_move(board, move):
-    # If any square is off the board, return false
-    if any(min(x, y) < 0 or max(x, y) > 7 for (x, y) in move):
-        return False
-    
-    # If starting square is not an owned piece, return false
-    if board[move[0][1]][move[0][0]] not in (1, 10):
-        return False
-    
-    # If any intermediate square is occupied, return false
-    if any(board[y][x] != 0 for (x, y) in move[1:]):
-        return False
-
-    # If move is a basic advance, check it
-    if len(move) == 2 and abs(move[0][0] - move[1][0]) == 1:
-        return is_legal_advance(board, move)
-
-    # Check jump legality
-    for start, end in [(move[i], move[i+1]) for i in range(len(move) - 1)]:
-        print "check jump legality", start, end
-        if not is_legal_jump(board, [start, end]):
-            return False
-
-    return True
 
 
 
@@ -99,7 +19,7 @@ if __name__ == "__main__":
         [0, 0, 2, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
         [2, 0, 2, 0, 2, 0, 2, 0],
-        [0, 2, 0, 2, 0, 0, 0, 2],
+        [0, 0, 0, 2, 0, 0, 0, 2],
         [2, 0, 2, 0, 2, 0, 2, 0]
     ]
 
